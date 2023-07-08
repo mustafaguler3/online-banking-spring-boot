ac2603160f6dae648c0421ce274a6769c193b8d6
mustafaguler3 <mustafa.glr@hotmail.com>
2023-07-08 12:28:07 +0300

    added primary account entity

---
+++ src/main/java/com/example/demo/domain/PrimaryAccount.java
@@ -0,0 +1,28 @@
+package com.example.demo.domain;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class PrimaryAccount {
+
+    private Long id;
+    private int accountNumber;
+    private BigDecimal accountBalance;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
